4817ca41b795eb70bef227973e5b357cbd1e80f6
Eduardo Nagano <eduardo_nagano@yahoo.com.br>
2024-12-20 10:45:15 -0300

    005

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,27 @@
 @extends('layouts/main_layout')
 @section('content')
+    {{-- Instrução IF simples --}}
+    @if ($value == 100)
+        <h1>Primeira parte do código</h1>
+    @endif
 
-    {{-- Comentário de blade --}}
+    {{-- Instrução IF ELSE --}}
+    @if ($value != 100)
+        <h1>Primeira parte do código</h1>
+    @else
+        <h1>Segunda parte do código</h1>
+    @endif
 
-    <p>PHP: <?= $name ?></p>
-    <p>PHP2: <?php echo $name ?></p>
-
-    <p>Blade: {{ $name }}</p>
-
+    {{-- Instrução IF ELSEIF ELSE --}}
+    @if ($value < 10)
+        <p>Primeiro</p>
+    @elseif($value < 20)
+        <p>Segundo</p>
+    @elseif($value > 50)
+        <p>Terceiro</p>
+    @elseif($value == 100)
+        <p>Quarto</p>
+    @else
+        <p>Outro caso</p>
+    @endif
 @endsection
