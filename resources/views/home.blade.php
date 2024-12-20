c6e782d2d070c007b3c460bd7aeeb3e1431dca2f
Eduardo Nagano <eduardo_nagano@yahoo.com.br>
2024-12-20 16:41:42 -0300

    012

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,6 @@
 @extends('layouts/main_layout')
 @section('content')
-    @production
-        <p>Estou em ambiente de produção.</p>
-    @else
-        <p>Não estou em produção.</p>
-        <p>{{ $value }}</p>
-    @endproduction
-
-    @env('local', 'development')
-    <p>Estou no ambiente: {{ env('APP_ENV') }}</p>
-    @endenv
-
-    {{-- formulário --}}
-    <form action="{{ route('submit') }}" method="POST">
-        @csrf
-        <div>
-            <input type="text" name="name">
-            @error('name')
-                <div class="alert alert-danger mt-2">{{ $message }}</div>
-            @enderror
-        </div>
-
-        <div>
-            <input type="text" name="country">
-            @error('country')
-                <div class="alert alert-danger mt-2">{{ $message }}</div>
-            @enderror
-        </div>
-
-        <button type="submit">Enviar</button>
-
-    </form>
+    @session('name')
+        <h3>A sessão tem o valor {{ session('name') }}</h3>
+    @endsession
 @endsection
